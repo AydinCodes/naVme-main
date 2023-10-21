e0e62da101ae73e1bc0a830052e84721b33fea32
Aydin Elhan <aydinmate@protonmail.com>
2023-10-22 08:10:24 +1100

    jobs list styling fixed

--- components/job-list.tsx
+++ components/job-list.tsx
@@ -31,9 +31,11 @@ const JobList: React.FC<JobListProps> = ({ className, jobs }) => {
         <div className=" h-[6rem] md:mb-8 flex flex-col justify-between items-center w-full">
           <div className=" w-full flex flex-col md:flex-row justify-between items-center">
             <div className="flex flex-col space-y-4">
-              <Label className="text-secondary-foreground mb-3 md:mb-0">
-                Total Jobs: {jobs ? jobs.length : '0'}
-              </Label>
+              {jobs && (jobs.length > 0) ? (
+                <Label className="text-secondary-foreground mb-3 md:mb-0">
+                  Total Jobs: {jobs.length}
+                </Label>
+              ) : ""}
             </div>
             <div className="flex flex-row space-x-4 items-center">
               <AddDialog />
@@ -53,11 +55,11 @@ const JobList: React.FC<JobListProps> = ({ className, jobs }) => {
         </div>
       </div>
       {loading ? (
-        <div className="flex justify-center mt-[4rem] mb-[2rem]">
+        <div className="flex justify-center mt-[4rem] mb-[2rem] h-full">
           <Loading />
         </div>
       ) : (
-        <ScrollArea className="mt-[2rem] md:mt-0 mb-6 md:mb-0 ">
+        <ScrollArea className="mt-[2rem] md:mt-0 mb-6 md:mb-0 h-full">
           <div className="flex flex-col space-y-[2rem] md:mr-4">
             {jobs?.map((job) => (
               <Job key={job.jobId} job={job} />
